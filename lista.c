5afd2df39075478493739642cbfc673787749293
VtSilveira <vitor.silveiravtss@gmail.com>
2022-04-07 13:16:28 -0300

    Documentacao da lista

--- lista.c
+++ lista.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+//Essa função apenas inicializa a Lista, criando o Nó sentinela e fazendo suas referencias anterior e próximo apontarem para si mesmo, uma vez que a Lista está vazia e é circular.
 void inicializarLista(lista *l) {
   NodeL *novo = (NodeL*)malloc(sizeof(NodeL));
   l->sentinela = novo;
@@ -10,52 +10,55 @@ void inicializarLista(lista *l) {
   novo->antes = l->sentinela;
 }
 
+//Destruição da Lista baseada nas funções vazia e removerInicio (poderia ser removerFim também).
 void destruirLista(lista *l) {
-  while (!vaziaLista(l))
-    removerInicioLista(l);
+  while (!vaziaLista(l))//enquanto a Lista não estiver vazia
+    removerInicioLista(l);//remove o nó do inicio
 
-  if (vaziaLista(l))
+  if (vaziaLista(l))//caso a Lista estiver vazia no final do loop, aparece a mensagem de sucesso, senão, a de erro.
     printf ("A lista foi destruida corretamente.\n");
   else
     printf ("Nao foi possível destruir a lista.\n");
 
-   free(l->sentinela);
+   free(l->sentinela);//desaloca o nó sentinela da memória que não é considerado como nó na função "removerInicio"
 }
 
+//checa se o próximo nó do sentinela é ele mesmo. Se sim, significa que ela está vazia.
 int vaziaLista(lista *l) {
   return l->sentinela->proximo == l->sentinela;
 }
 
+
 void removerInicioLista(lista *l) {
-  NodeL *aux = l->sentinela->proximo;
-  if (!vaziaLista(l)) {
-    aux->proximo->antes = aux->antes;
-    aux->antes->proximo = aux->proximo;
-    free(aux);
+  NodeL *aux = l->sentinela->proximo;//é criado um ponteiro para o inicio da fila
+  if (!vaziaLista(l)) {//se a lista não tiver vazia
+    aux->proximo->antes = aux->antes; // o nó anterior ao segundo antigo nó passa a ser o sentinela, tornando-o o primeiro nó da Lista
+    aux->antes->proximo = aux->proximo; //o nó posterior ao sentinela passa a ser o antigo segundo nó da Lista
+    free(aux);//o antigo primeiro nó da Lista é desalocado
   }
-  l->tam--;
+  l->tam--;//o tamanho da fila é decrementado
 }
 
 void removerFinalLista(lista *l) {
-  NodeL *aux = l->sentinela->antes;
-  if (!vaziaLista(l)) {
-    aux->proximo->antes = aux->antes;
-    aux->antes->proximo = aux->proximo;
-    free(aux);
+  NodeL *aux = l->sentinela->antes;//é criado um ponteiro para o fim da fila
+  if (!vaziaLista(l)) {//se a lista não tiver vazia
+    aux->proximo->antes = aux->antes;// o nó anterior ao penultimo antigo nó passa a ser o sentinela, tornando-o o ultimo nó da Lista
+    aux->antes->proximo = aux->proximo;//o nó posterior ao sentinela passa a ser o antigo penultimo nó da Lista, conservando a característica circular da Lista
+    free(aux);//o antigo ultimo nó da Lista é desalocado
   }
-  l->tam--;
+  l->tam--;//o tamanho da fila é decrementado
 }
 
 void inserirInicioLista(lista *l, int num) {
-  NodeL *novo = malloc(sizeof(NodeL));
+  NodeL *novo = malloc(sizeof(NodeL));//é alocado um espaço na memoria para o novo nó
 
-  if (novo) {
+  if (novo) {//se o novo nó foi alocado corretamente:
     novo->valor = num;
-    l->sentinela->proximo->antes = novo;
-    novo->proximo = l->sentinela->proximo;
-    l->sentinela->proximo = novo;
-    novo->antes = l->sentinela;
-    l->tam++;
+    l->sentinela->proximo->antes = novo;//a referencia "anterior" do antigo primeiro nó passa a ser o novo nó, uma vez que iremos inserir no inicio
+    novo->proximo = l->sentinela->proximo;//o proximo nó do novo nó passa a ser o antigo primeiro nó
+    l->sentinela->proximo = novo;// é colocado no inicio da Lista, sendo posterior ao sentinela
+    novo->antes = l->sentinela; // a fim de manter a característica circular da Lista, a referencia de anterior do novo nó passa a ser o sentinela
+    l->tam++;//tamanho da fila é incrementado
 
   } else {
     printf("A memoria não foi alocada corretamente\n");
@@ -65,20 +68,21 @@ void inserirInicioLista(lista *l, int num) {
 void inserirFinalLista(lista *l, int num) {
   NodeL *novo = malloc(sizeof(NodeL));
 
-  if (novo) {
+  if (novo) {//se o nó foi alocado corretamente:
     novo->valor = num;
-    novo->proximo = l->sentinela;
+    novo->proximo = l->sentinela;//o nó posterior ao novo nó passa a ser o sentinela
+    l->sentinela->antes = novo;// o novo nó passa a ser o nó anterior ao sentinela
     l->tam++;
 
-    if (vaziaLista(l)) {
-      l->sentinela->antes = novo;
+    if (vaziaLista(l)) {//se a lista estiver vazia:
+      //tanto o nó posterior como o anterior ao sentinela será o novo nó alocado
       l->sentinela->proximo = novo;
-      novo->antes = l->sentinela;
+      novo->antes = l->sentinela;// a fim de manter a característica circular da Lista, a referencia de anterior do novo nó passa a ser o sentinela, uma vez que inserir no fim nesse caso é a mesma coisa que inserir no inicio
 
-    } else {
-      l->sentinela->antes->proximo = novo;
-      novo->antes = l->sentinela->antes;
-      l->sentinela->antes = novo;
+    } else {// se ela não estiver vazia
+      l->sentinela->antes->proximo = novo;//a referencia "anterior" do antigo ultimo nó passa a ser o novo nó, uma vez que iremos inserir no fim
+      novo->antes = l->sentinela->antes; // o no anterior ao novo nó será o antigo ultimo nó da Lista
+      
     }
 
   } else {
@@ -132,9 +136,10 @@ void inserirFinalLista(lista *l, int num) {
   }
 }*/
 
+
 void imprimir(NodeL *nodeL) {
   printf("\nLista: ");
-  while (nodeL) {
+  while (nodeL) {//
     printf("%d ", nodeL->valor);
     nodeL = nodeL->proximo;
   }
--- lista.h
+++ lista.h
@@ -1,16 +1,19 @@
 #ifndef LISTA_H
 #define LISTA_H
 
+//O NodeL possui um espaço para um int, como também possui dois ponteiros em sua composição (proximo e anterior), que foram implementados com o intuito de cumprir com a o requisito "Lista duplamente encadeada".
 typedef struct nodeL {
   int valor;
   struct nodeL *proximo;
   struct nodeL *antes;
 } NodeL;
 
+//o struct Lista possui um ponteiro para o Nó sentinela e também um campo referente ao seu tamanho.
 typedef struct lista {
   NodeL *sentinela;
   int tam;
 } lista;
+
 
 void inicializarLista(lista *l);
 void inserirInicioLista(lista *l, int num);
